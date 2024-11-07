95f8e3f2f1d7ad99f05d7ee5063d860b1865ca02
shubham-johri85 <shubham.johri85@gmail.com>
2024-11-07 09:25:43 +0530

    Commit and Push

---
+++ Nov/ex_05112024_Python_Basics/Lab002_Python_Basics.py
@@ -0,0 +1,5 @@
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/2)
+# This is a comment (Ctrl+/)
---
+++ Nov/ex_05112024_Python_Basics/Lab003_Python_Basics.py
@@ -0,0 +1,7 @@
+print("Shubham",111, "Test", 222)
+# print(self, *args, sep=' ', end='\n', file=None): # known special case of print
+# *args - Unlimited number of comma seperated arguments
+print("Shubham", 123, sep="__" )
+    # print("Indentation") - IndentationError: unexpected indent
+print("Test", 222, end=" & " )
+print("Task Complete")
