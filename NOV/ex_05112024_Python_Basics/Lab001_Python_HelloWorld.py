64fa0f81cd70b88f5dd7d65e517f20f4e7a49260
shubham-johri85 <shubham.johri85@gmail.com>
2024-11-07 08:49:02 +0530

    Delete NOV directory

--- NOV/ex_05112024_Python_Basics/Lab001_Python_HelloWorld.py
+++
@@ -1,4 +0,0 @@
-print("Hello World")
-print("Start Python Automation")
-# This is a comment
-
--- NOV/ex_05112024_Python_Basics/Lab003_Python_Basics.py
+++
@@ -1,7 +0,0 @@
-print("Hi, Shubham")
-# print(self, *args, sep=' ', end='\n', file=None):
-# *args - Unlimited number of comma seperated arguments
-print("Shubham", 111, "PyATB5x", 5, "NOV")
-         # print("Shubham", 111, "PyATB5x", 5, "NOV",sep="-") IndentationError: unexpected indent
-print("Shubham", 111, "PyATB5x", 5, "NOV",sep=" ", end="__")
-print("Shubham", 111, "PyATB5x", 5, "NOV",sep="$", end="_Task Complete")
