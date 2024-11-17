ac613127f2ec69d441dbcff5806a3004393c8f14
shubham-johri85 <shubham.johri85@gmail.com>
2024-11-17 16:18:42 +0530

    16th November

--- Nov/ex_12112024_Operators/Lab044_Task 1.py
+++ Nov/ex_12112024_Operators/Lab044_Task 1.py
@@ -1,4 +1,4 @@
-# Write a program to take user age and let him know if he can go to club
+# Write a program to take user age and let him know if he can go to club. Age = 21
 
 user_age = int(input("Enter your age in years\n"))
 
--- Nov/ex_12112024_Operators/Lab046_Task 3.py
+++ Nov/ex_12112024_Operators/Lab046_Task 3.py
@@ -1,9 +1,12 @@
 # Write a program to find maximum of two numbers using ternary operator
 
-num1 = int(input("Enter first number--> "))
-num2 = int(input("Enter second number--> "))
+num1 = float(input("Enter first number--> "))
+num2 = float(input("Enter second number--> "))
 
-if num1 > num2:
+if num1 >= num2:
     print("The maximum number is--> ", num1)
 else:
     print("The maximum number is--> ", num2)
+
+
+# If user enter string like one or two, this will be done by "try and except"
